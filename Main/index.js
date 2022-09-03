a913d70f348e2f681c262708d112d836f64b3f8e
calingoia2001 <calingoia01@yahoo.com>
2022-09-03 13:59:01 +0300

    Commit4 basic bubblesort vizualizer

--- Main/index.js
+++ Main/index.js
@@ -3,28 +3,63 @@ const sort_btn = document.getElementById('sort-btn');
 const rst_btn = document.getElementById('rst-btn');
 const randomize_arr = document.getElementById('randomize-arr-btn');
 
-let arrLength = 10;
+let arrLength = 100;
 
+function randomUnsortedArray() {
+    unsortedArr = Array.from({ length: arrLength }, () => Math.floor(Math.random() * 40));
+}
 
 function renderBars(array) {
     for(let i = 0; i < arrLength; i++) {
         let bar = document.createElement("div");
         bar.classList.add("bar");
         bar.style.height = array[i] + "px";
-        bar.style.backgroundColor = "blue";
+        bar.style.backgroundColor = "lightgreen";
         bar.innerText = array[i];
         bars_container.appendChild(bar);
     }
 }
 
+function sleep(ms) { return new Promise((resolve) => setTimeout(resolve, ms))};
 
+async function bubbleSort(array) {
+    let bars = document.getElementsByClassName("bar");
+    for(let i = 0; i < arrLength; i++) {
+        for(let j = 0; j < arrLength; j++) {
+            if(array[j] > array[j + 1]) {
 
-sort_btn.addEventListener('click', () => {
-    
-})
+                /*for(let k = 0 ; k < bars.length; k++) {
+                    if(k !== j && k !== j + 1) {
+                        bars[k].style.backgroundColor = "white";
+                    }
+                } */
+
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                bars[j].style.height = array[j] * 10 + "px";
+                bars[j].style.backgroundColor = "lightgreen";
+                bars[j].innerText = array[j];
+
+                bars[j + 1].style.height = array[j + 1] * 10 + "px";
+                bars[j + 1].style.backgroundColor = "lightgreen";
+                bars[j + 1].innerText = array[j + 1];
+                await sleep(100);
+            }
+        }
+        await sleep(100);
+    }
+    return array;
+}
 
 randomize_arr.addEventListener('click', () => {
-    let array = Array.from({ length: arrLength }, () => Math.floor(Math.random() * 40));
-    bars_container.innerHTML = "";
-    renderBars(array);
+    randomUnsortedArray();
+    bars_container.innerHTML = ""; 
+    renderBars(unsortedArr);
 });
+
+sort_btn.addEventListener('click', () => {
+    bubbleSort(unsortedArr);
+    console.log(unsortedArr);
+})
